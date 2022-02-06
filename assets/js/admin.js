98836db3a93b4d5ff8959b11c7a899aab70c7b0d
Muhammad Idrees <midexigner@gmail.com>
2022-02-07 01:19:47 +0500

    added some pages like user-new users etc

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,5 +1,6 @@
  $(document).ready(function () {
-    $(".sidebar-menu-dropdown:not(.submenu-active)").hover(function () {
+    $(".menu-dropdown:not(.submenu-active)").hover(function () {
       $(this).toggleClass("submenu-hover");
     });
+   
   });
